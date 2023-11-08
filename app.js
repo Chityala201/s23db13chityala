c4c1d02820727cde64c3abfb26a76a54312cc6fc
Chityala201 <143207403+Chityala201@users.noreply.github.com>
2023-11-08 17:46:13 -0600

    cereals updated in all routes

--- app.js
+++ app.js
@@ -4,11 +4,27 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+const connectionString =
+process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString);
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")});
+
+var cereal = require("./models/cerealSchema");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var cerealRouter = require('./routes/cereal');
 var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
+var resourceRouter = require('./routes/resource');
+var costumeRouter = require('./routes/costumes');
 
 var app = express();
 
@@ -27,6 +43,46 @@ app.use('/users', usersRouter);
 app.use('/cereal', cerealRouter);
 app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
+app.use('/resource', resourceRouter);
+app.use('/costumes' , costumeRouter);
+
+// We can seed the collection if needed on
+
+async function recreateDB(){
+// Delete everything
+await cereal.deleteMany();
+
+let instance1 = new
+cereal({cerealBrandName:"Kellogg's", cerealFlavor:'Blue Berries',
+price:5.8});
+instance1.save().then(doc=>{
+console.log("First object saved")}
+).catch(err=>{
+console.error(err)
+});
+
+let instance2 = new
+cereal({cerealBrandName:"Grains", cerealFlavor:'Peanut',
+price:7.8});
+instance2.save().then(doc=>{
+console.log("Second object saved")}
+).catch(err=>{
+console.error(err)
+});
+
+let instance3 = new
+cereal({cerealBrandName:"Lucky chance", cerealFlavor:'Vanila',
+price:6.8});
+instance3.save().then(doc=>{
+console.log("Third object saved")}
+).catch(err=>{
+console.error(err)
+});
+
+}
+let reseed = true;
+if (reseed) {recreateDB();}
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ models/cerealSchema.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose")
+const cerealSchema = mongoose.Schema({
+cerealBrandName: String,
+cerealFlavor: String,
+price: Number
+})
+module.exports = mongoose.model("cereal",
+cerealSchema)
